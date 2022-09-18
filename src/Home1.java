fbbcd8aa09179b8acfb9c2640c467fab56b7d33e
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-18 14:53:00 +0300

    sayı bulma
    sayı bulma

--- src/Home1.java
+++ src/Home1.java
@@ -17,14 +17,26 @@ public class Home1 {
 		System.out.println("mükemmel sayıdır");	
 		}else {
 			System.out.println("mükemmel sayı değildir");
+			
 		}
 			
-			
-			
+	int[] sayılar = new int [] {1,2,5,7,9,0};
+	int aranacak = 5;
+	boolean varMı=false;
+	 for (int sayı:sayılar) {
+		 if(sayı==aranacak) {
+			 varMı=true;
+			 break;
+		 }
+		
+	 }
 			
 		
-			
-	
+	if	(varMı)	{
+	System.out.println("sayı mevcuttur");
+	}else {
+	System.out.println("sayı mevcut değildir");
+	}
 		
 			
 		
